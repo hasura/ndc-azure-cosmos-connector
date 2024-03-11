f0dbbdfba39eeac820eb9ba2bb8709aef944ff58
Karthikeyan C <karthikeyan@hasura.io>
2024-03-11 14:58:04 +0530

    Remove mocha and add jest as testing framework

--- tests/init.ts
+++
@@ -1,8 +0,0 @@
-// This file is hooked into mocha via .mocharc.json
-
-import chai from "chai";
-import chaiAsPromised from "chai-as-promised"
-
-export const mochaGlobalSetup = function() {
-    chai.use(chaiAsPromised);
-};
