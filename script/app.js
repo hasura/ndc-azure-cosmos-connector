8b76c4c40471bffa5e3969d64e53563d944def9d
Pranshi <pranshi@hasura.io>
2024-03-19 15:40:12 +0530

    add script to create db and add data in the emulator

---
+++ script/app.js
@@ -0,0 +1,32 @@
+import { CosmosClient } from '@azure/cosmos'
+
+const cosmosClient = new CosmosClient({
+    endpoint: 'https://localhost:8081/',
+    key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+})
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+const { database } = await cosmosClient.databases.createIfNotExists({
+    id: 'ConnectorTest',
+    throughput: 400
+  })
+  
+  const { container } = await database.containers.createIfNotExists({
+    id: 'NobelLaureates',
+    partitionKey: {
+      paths: [
+        '/year'
+      ]
+    }
+  })
+
+import prizes_data from './data/data.json' assert { type: 'json' };
+
+var i = 0;
+
+for (const prize of prizes_data) {
+    container.items.upsert(prize)
+    i++;
+}
+console.log(i)
