83e04d758226c5bbe22424a1b840534329f28cf8
Pranshi <pranshi@hasura.io>
2024-03-22 20:43:49 +0530

    wip: github ci pipeline

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 import { CosmosClient } from '@azure/cosmos'
 
 const cosmosClient = new CosmosClient({
-    endpoint: 'https://localhost:8081/',
+    endpoint: 'https://localhost:8082/',
     key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
 })
 
