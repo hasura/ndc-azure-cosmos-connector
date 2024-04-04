e292372c557a5bf0ac05f5f10a8899ffe7c1824d
Karthikeyan C <karthikeyan@hasura.io>
2024-04-04 13:36:04 +0530

    try installing the emulator certificate

--- script/app.js
+++ script/app.js
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 
 
 async function main() {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    //process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
     const cosmosClient = new CosmosClient({
         endpoint: 'https://127.0.0.1:8081/',
