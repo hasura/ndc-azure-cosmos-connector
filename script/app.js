6a29e7536aaf9dc9099eb0e8cea067d24e2d97b7
Karthikeyan C <karthikeyan@hasura.io>
2024-04-04 13:33:02 +0530

    Install azure identity as well

--- script/app.js
+++ script/app.js
@@ -8,7 +8,7 @@ async function main() {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
     const cosmosClient = new CosmosClient({
-        endpoint: 'https://localhost:8081/',
+        endpoint: 'https://127.0.0.1:8081/',
         key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
     });
 
