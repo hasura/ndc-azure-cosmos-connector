71602c158ba9f31fdd40428284ce44aac4319dad
Karthikeyan C <karthikeyan@hasura.io>
2024-04-04 13:54:17 +0530

    more changes to make the CI work

--- script/app.js
+++ script/app.js
@@ -9,7 +9,10 @@ async function main() {
 
     const cosmosClient = new CosmosClient({
         endpoint: 'https://127.0.0.1:8081/',
-        key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+        key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
+        agent: new https.Agent({
+            rejectUnauthorized: false
+        })
     });
 
     const { database } = await cosmosClient.databases.createIfNotExists({
