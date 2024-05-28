cba83e6b2dc8b74be98f518f1686f6d05436d167
Karthikeyan C <karthikeyan@hasura.io>
2024-05-28 12:39:25 +0530

    Add command to run the connector from the CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,17 +2,21 @@
 
 import { Command, Option } from "commander";
 import * as updateCmd from "./update";
-import * as startCmd from "./start"
 import { execSync } from "child_process";
 import { readFileSync } from "fs";
-import { ConnectorConfig } from "../connector";
+import { ConnectorConfig, createConnector } from "../connector";
 import { resolve } from "path";
+import { getServeCommand } from "@hasura/ndc-sdk-typescript";
 
-export const program = new Command()
-    .version("0.0.1")
-    .description("Azure Cosmos Connector CLI")
-    // .addCommand(initCmd.cmd) TODO: Enable when required by the CLI spec
-    .addCommand(updateCmd.cmd)
-    .addCommand(startCmd.cmd)
+function main() {
+    const program = getServeCommand();
 
-program.parse(process.argv);
+    program.version("0.0.1")
+        .description("Azure Cosmos Connector CLI")
+        .addCommand(updateCmd.cmd);
+
+    program.addCommand(getServeCommand(createConnector()));
+    program.parse(process.argv);
+}
+
+main()
