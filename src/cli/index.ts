1f3e5010d3bad282b9e1a38512b68296575e5899
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2024-08-20 15:00:49 +0530

    Parse the hasura options in the CLI correctly (#36)
    * fix bug in parsing the CLI options
    
    * create config.json
    
    * add the update command back

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,35 +6,6 @@ import { createConnector } from "../connector";
 import { version } from "../../package.json"
 import * as sdk from "@hasura/ndc-sdk-typescript";
 
-function main() {
-    const program = new Command().name("ndc-azure-cosmos").version(version);
-
-    program.addCommand(updateCmd.cmd);
-
-    program.action(async (options: sdk.ServerOptions) => {
-        program.addOption(
-            new Option("--configuration <directory>")
-                .env("HASURA_CONFIGURATION_DIRECTORY")
-                .default(".")
-        )
-        program.addOption(
-            new Option("--port <port>")
-                .env("HASURA_CONNECTOR_PORT")
-                .default(8080)
-                .argParser(parseIntOption)
-        )
-        program.addOption(
-            new Option("--service-token-secret <secret>").env("HASURA_SERVICE_TOKEN_SECRET")
-        )
-        program.addOption(new Option("--log-level <level>").env("HASURA_LOG_LEVEL").default("info"))
-        program.addOption(new Option("--pretty-print-logs").env("HASURA_PRETTY_PRINT_LOGS").default(false))
-
-        const connector = createConnector();
-        await sdk.startServer(connector, options)
-    });
-
-    program.parse(process.argv);
-}
 
 function parseIntOption(value: string, _previous: number): number {
     // parseInt takes a string and a radix
@@ -45,4 +16,55 @@ function parseIntOption(value: string, _previous: number): number {
     return parsedValue;
 }
 
-main()
+
+function main() {
+    const program = new Command().name("ndc-azure-cosmos").version(version);
+
+    // Define the options
+    program.addOption(
+        new Option("--configuration <directory>")
+            .env("HASURA_CONFIGURATION_DIRECTORY")
+            .default(".")
+    );
+    program.addOption(
+        new Option("--port <port>")
+            .env("HASURA_CONNECTOR_PORT")
+            .default(8080)
+            .argParser(parseIntOption)
+    );
+    program.addOption(
+        new Option("--host <host>")
+            .env("HASURA_CONNECTOR_HOST")
+            .default("::")
+    );
+    program.addOption(
+        new Option("--service-token-secret <secret>")
+            .env("HASURA_SERVICE_TOKEN_SECRET")
+    );
+    program.addOption(
+        new Option("--log-level <level>")
+            .env("HASURA_LOG_LEVEL")
+            .default("info")
+    );
+    program.addOption(
+        new Option("--pretty-print-logs")
+            .env("HASURA_PRETTY_PRINT_LOGS")
+            .default(false)
+    );
+
+    program.addCommand(updateCmd.cmd);
+
+    // Define the action
+    program.action(async (options: sdk.ServerOptions) => {
+        const connector = createConnector();
+        console.log("Starting server with options...", options);
+        await sdk.startServer(connector, options);
+    });
+
+
+
+    // Parse the command line arguments
+    program.parse(process.argv);
+}
+
+main();
