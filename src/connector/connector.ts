8a871f6c7f51891fca4e1953108cffdc19ab3512
Karthikeyan C <karthikeyan@hasura.io>
2024-09-06 19:49:36 +0530

    Change the file structure

---
+++ src/connector/connector.ts
@@ -0,0 +1,121 @@
+import * as sdk from "@hasura/ndc-sdk-typescript";
+import { CollectionsSchema, getNdcSchemaResponse } from "./schema";
+import { constructCosmosDbClient } from "./db/cosmosDb";
+import { Database } from "@azure/cosmos";
+import { executeQuery } from "./execution";
+import { readFileSync } from "fs";
+
+export type Configuration = ConnectorConfig;
+
+export type ConnectorConfig = {
+  connection: {
+    endpoint: string;
+    key: string;
+    databaseName: string;
+  };
+  schema: CollectionsSchema;
+};
+
+export type State = {
+  databaseClient: Database;
+};
+
+export function createConnector(): sdk.Connector<Configuration, State> {
+  const connector: sdk.Connector<Configuration, State> = {
+    parseConfiguration: async function (
+      configurationDir: string,
+    ): Promise<Configuration> {
+      try {
+        const configLocation = `${configurationDir}/config.json`;
+        const fileContent = readFileSync(configLocation, "utf8");
+        const configObject: ConnectorConfig = JSON.parse(fileContent);
+        return Promise.resolve(configObject);
+      } catch (error) {
+        console.error("Failed to parse configuration:", error);
+        throw new sdk.InternalServerError(
+          "Internal Server Error, server configuration is invalid",
+          {},
+        );
+      }
+    },
+
+    tryInitState: async function (
+      _: Configuration,
+      __: unknown,
+    ): Promise<State> {
+      try {
+        const databaseClient = constructCosmosDbClient().dbClient;
+        return Promise.resolve({
+          databaseClient,
+        });
+      } catch (error) {
+        console.error("Failed to initialize the state of the connector", error);
+        throw new sdk.InternalServerError(
+          `Internal server error, failed to initialize the state of the connector - ${error}`,
+          {},
+        );
+      }
+    },
+
+    getSchema: async function (
+      configuration: Configuration,
+    ): Promise<sdk.SchemaResponse> {
+      if (!configuration.schema) {
+        throw new sdk.Forbidden(
+          "Internal server error, server configuration not found",
+        );
+      }
+      return Promise.resolve(getNdcSchemaResponse(configuration.schema));
+    },
+
+    getCapabilities(_: Configuration): sdk.Capabilities {
+      return {
+        query: {
+          nested_fields: {},
+        },
+        mutation: {},
+      };
+    },
+
+    query: async function (
+      configuration: Configuration,
+      state: State,
+      request: sdk.QueryRequest,
+    ): Promise<sdk.QueryResponse> {
+      return executeQuery(request, configuration.schema, state.databaseClient);
+    },
+
+    mutation: async function (
+      configuration: Configuration,
+      state: State,
+      request: sdk.MutationRequest,
+    ): Promise<sdk.MutationResponse> {
+      throw new Error("Not implemented");
+    },
+
+    queryExplain: function (
+      configuration: Configuration,
+      state: State,
+      request: sdk.QueryRequest,
+    ): Promise<sdk.ExplainResponse> {
+      throw new Error("Function not implemented.");
+    },
+
+    mutationExplain: function (
+      configuration: Configuration,
+      state: State,
+      request: sdk.MutationRequest,
+    ): Promise<sdk.ExplainResponse> {
+      throw new Error("Function not implemented.");
+    },
+
+    fetchMetrics: async function (
+      configuration: Configuration,
+      state: State,
+    ): Promise<undefined> {
+      return undefined;
+    },
+  };
+
+  return connector;
+}
