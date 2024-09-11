fd33f923310d4f50995bcd8b27701991ab690023
Karthikeyan C <karthikeyan@hasura.io>
2024-09-11 16:50:44 +0530

    fix the imports

--- src/cli/config.ts
+++ src/cli/config.ts
@@ -1,42 +1,56 @@
 import { Database } from "@azure/cosmos";
-import { CollectionDefinition, CollectionDefinitions, CollectionsSchema, NamedObjectTypeDefinition, ScalarTypeDefinitions, getJSONScalarTypes } from "../schema"
-import { BuiltInScalarTypeName, ObjectTypeDefinitions, TypeDefinition, ObjectTypePropertiesMap } from "../schema";
+import {
+  CollectionDefinition,
+  CollectionDefinitions,
+  CollectionsSchema,
+  NamedObjectTypeDefinition,
+  ScalarTypeDefinitions,
+  getJSONScalarTypes,
+} from "../connector/schema";
+import {
+  BuiltInScalarTypeName,
+  ObjectTypeDefinitions,
+  TypeDefinition,
+  ObjectTypePropertiesMap,
+} from "../connector/schema";
 
-import { Container } from "@azure/cosmos"
+import { Container } from "@azure/cosmos";
 
-import { InputData, jsonInputForTargetLanguage, quicktype } from "quicktype-core";
-import { runSQLQuery, constructCosmosDbClient } from "../cosmosDb"
+import {
+  InputData,
+  jsonInputForTargetLanguage,
+  quicktype,
+} from "quicktype-core";
+import { runSQLQuery, constructCosmosDbClient } from "../connector/db/cosmosDb";
 import { exit } from "process";
 import fs from "fs";
 import { promisify } from "util";
 import { $RefParser } from "@apidevtools/json-schema-ref-parser";
 
-
 export type RawConfiguration = {
-    azure_cosmos_key: string,
-    azure_cosmos_db_endpoint: string,
-    azure_cosmos_db_name: string,
-    azure_cosmos_no_of_rows_to_fetch: number | null
-}
+  azure_cosmos_key: string;
+  azure_cosmos_db_endpoint: string;
+  azure_cosmos_db_name: string;
+  azure_cosmos_no_of_rows_to_fetch: number | null;
+};
 
 type JSONSchemaProperty = {
-    type: string,
-    $ref: string,
-    items?: JSONSchemaProperty
-}
+  type: string;
+  $ref: string;
+  items?: JSONSchemaProperty;
+};
 
 type JSONSchemaDefinition = {
-    type: string,
-    additionalProperties: boolean,
-    properties?: Record<string, JSONSchemaProperty>,
-    title: string,
-}
+  type: string;
+  additionalProperties: boolean;
+  properties?: Record<string, JSONSchemaProperty>;
+  title: string;
+};
 
 export type JSONSchema = {
-    definitions: Record<string, JSONSchemaDefinition>,
-    $ref: string,
-}
-
+  definitions: Record<string, JSONSchemaDefinition>;
+  $ref: string;
+};
 
 /**
    * Fetches at-most `n` latest updated rows from the given container
@@ -46,144 +60,162 @@ export type JSONSchema = {
    * @returns The latest at-most `n` rows from the `container`.
 
 **/
-export async function fetchLatestNRowsFromContainer(n: number, container: Container): Promise<string[]> {
-    const querySpec = {
-        query: `SELECT * FROM ${container.id} c ORDER BY c._ts DESC OFFSET 0 LIMIT ${n}`,
-        parameters: []
-    }
+export async function fetchLatestNRowsFromContainer(
+  n: number,
+  container: Container,
+): Promise<string[]> {
+  const querySpec = {
+    query: `SELECT * FROM ${container.id} c ORDER BY c._ts DESC OFFSET 0 LIMIT ${n}`,
+    parameters: [],
+  };
 
-    return await runSQLQuery<string>(querySpec, container)
+  return await runSQLQuery<string>(querySpec, container);
 }
 
+export async function inferJSONSchemaFromContainerRows(
+  rows: string[],
+  containerTypeName: string,
+): Promise<JSONSchema> {
+  const jsonInput = jsonInputForTargetLanguage("schema");
 
-export async function inferJSONSchemaFromContainerRows(rows: string[], containerTypeName: string): Promise<JSONSchema> {
-    const jsonInput = jsonInputForTargetLanguage("schema");
+  await jsonInput.addSource({
+    name: containerTypeName,
+    samples: rows.map((x) => JSON.stringify(x)),
+  });
 
-    await jsonInput.addSource({
-        name: containerTypeName,
-        samples: rows.map(x => JSON.stringify(x))
-    });
+  const inputData = new InputData();
+  inputData.addInput(jsonInput);
 
-    const inputData = new InputData();
-    inputData.addInput(jsonInput);
+  let jsonSchema = await quicktype({
+    inputData,
+    lang: "schema",
+  });
 
-    let jsonSchema = await quicktype({
-        inputData,
-        lang: "schema"
-    });
+  let rawJSONSchemaOutput: any = jsonSchema.lines.join("\n");
 
-    let rawJSONSchemaOutput: any = jsonSchema.lines.join("\n");
-
-    return JSON.parse(rawJSONSchemaOutput)
-
+  return JSON.parse(rawJSONSchemaOutput);
 }
 
-function getPropertyTypeDefn(property: JSONSchemaProperty, $refs: $RefParser): TypeDefinition | null {
-    if (property.$ref !== undefined && property.$ref !== null) {
+function getPropertyTypeDefn(
+  property: JSONSchemaProperty,
+  $refs: $RefParser,
+): TypeDefinition | null {
+  if (property.$ref !== undefined && property.$ref !== null) {
+    const referencedPropertyDefn = $refs.$refs.get(
+      property.$ref,
+    ) as JSONSchemaDefinition;
 
-        const referencedPropertyDefn = $refs.$refs.get(property.$ref) as JSONSchemaDefinition;
-
-        if (referencedPropertyDefn.type === "object") {
-            return {
-                type: "named",
-                name: referencedPropertyDefn.title,
-                kind: "object"
-            }
-        } else if (referencedPropertyDefn.type === "string") {
-            return {
-                type: "named",
-                name: BuiltInScalarTypeName.String,
-                kind: "scalar"
-            }
-        } else {
-            console.log("Warning: Could not infer the type for referenced property", property);
-
-        }
-
-
-    } else if (property.type == "null") {
-        // We don't have enough information to predict anything about the property. So, just
-        // return null.
-        return null
-    } else if (property.type == "array") {
-        if (property.items !== undefined) {
-            const elementType = getPropertyTypeDefn(property.items, $refs);
-
-            if (elementType !== null) {
-                return {
-                    "type": "array",
-                    "elementType": elementType
-                }
-            }
-
-        }
-        return null
-    } else if (property.type == "string") {
-        return {
-            "type": "named",
-            name: BuiltInScalarTypeName.String,
-            kind: "scalar"
-        }
-    } else if (property.type == "number") {
-        return {
-            "type": "named",
-            name: BuiltInScalarTypeName.Number,
-            kind: "scalar"
-        }
+    if (referencedPropertyDefn.type === "object") {
+      return {
+        type: "named",
+        name: referencedPropertyDefn.title,
+        kind: "object",
+      };
+    } else if (referencedPropertyDefn.type === "string") {
+      return {
+        type: "named",
+        name: BuiltInScalarTypeName.String,
+        kind: "scalar",
+      };
+    } else {
+      console.log(
+        "Warning: Could not infer the type for referenced property",
+        property,
+      );
     }
-    else if (property.type == "integer") {
-        return {
-            "type": "named",
-            name: BuiltInScalarTypeName.Number,
-            kind: "scalar"
-        }
-    } else if (property.type == "boolean") {
-        return {
-            "type": "named",
-            name: BuiltInScalarTypeName.Boolean,
-            kind: "scalar"
-        }
-    }
+  } else if (property.type == "null") {
+    // We don't have enough information to predict anything about the property. So, just
+    // return null.
+    return null;
+  } else if (property.type == "array") {
+    if (property.items !== undefined) {
+      const elementType = getPropertyTypeDefn(property.items, $refs);
 
-    return null
+      if (elementType !== null) {
+        return {
+          type: "array",
+          elementType: elementType,
+        };
+      }
+    }
+    return null;
+  } else if (property.type == "string") {
+    return {
+      type: "named",
+      name: BuiltInScalarTypeName.String,
+      kind: "scalar",
+    };
+  } else if (property.type == "number") {
+    return {
+      type: "named",
+      name: BuiltInScalarTypeName.Number,
+      kind: "scalar",
+    };
+  } else if (property.type == "integer") {
+    return {
+      type: "named",
+      name: BuiltInScalarTypeName.Number,
+      kind: "scalar",
+    };
+  } else if (property.type == "boolean") {
+    return {
+      type: "named",
+      name: BuiltInScalarTypeName.Boolean,
+      kind: "scalar",
+    };
+  }
+
+  return null;
 }
 
-export async function getObjectTypeDefinitionsFromJSONSchema(containerJSONSchema: JSONSchema): Promise<ObjectTypeDefinitions> {
-    var objectTypeDefinitions: ObjectTypeDefinitions = {};
-    let parser = new $RefParser();
+export async function getObjectTypeDefinitionsFromJSONSchema(
+  containerJSONSchema: JSONSchema,
+): Promise<ObjectTypeDefinitions> {
+  var objectTypeDefinitions: ObjectTypeDefinitions = {};
+  let parser = new $RefParser();
 
-    const $refs = await parser.resolve(JSON.parse(JSON.stringify(containerJSONSchema)));
-    Object.entries(containerJSONSchema.definitions).forEach(([objectTypeName, objectTypeDefinition]) => {
-        if (objectTypeDefinition.type == "object") {
-            var objectTypeProperties: ObjectTypePropertiesMap = {};
+  const $refs = await parser.resolve(
+    JSON.parse(JSON.stringify(containerJSONSchema)),
+  );
+  Object.entries(containerJSONSchema.definitions).forEach(
+    ([objectTypeName, objectTypeDefinition]) => {
+      if (objectTypeDefinition.type == "object") {
+        var objectTypeProperties: ObjectTypePropertiesMap = {};
 
-            if (objectTypeDefinition.properties !== undefined) {
+        if (objectTypeDefinition.properties !== undefined) {
+          Object.entries(objectTypeDefinition.properties).map(
+            ([propertyName, propertyDefn]) => {
+              let propertyTypeDefn = getPropertyTypeDefn(propertyDefn, parser);
 
-                Object.entries(objectTypeDefinition.properties).map(([propertyName, propertyDefn]) => {
+              let legacyProperty = [
+                "_rid",
+                "_self",
+                "_etag",
+                "_attachments",
+                "_ts",
+              ];
+              if (
+                propertyTypeDefn !== null &&
+                !legacyProperty.includes(propertyName)
+              ) {
+                objectTypeProperties[propertyName] = {
+                  propertyName: propertyName,
+                  description: null,
+                  type: propertyTypeDefn,
+                };
+              }
+            },
+          );
 
-                    let propertyTypeDefn = getPropertyTypeDefn(propertyDefn, parser);
-
-                    let legacyProperty = ['_rid', '_self', '_etag', '_attachments', '_ts']
-                    if (propertyTypeDefn !== null && !legacyProperty.includes(propertyName)) {
-                        objectTypeProperties[propertyName] = {
-                            propertyName: propertyName,
-                            description: null,
-                            type: propertyTypeDefn
-                        };
-                    }
-
-                })
-
-                objectTypeDefinitions[objectTypeName] = {
-                    description: null,
-                    properties: objectTypeProperties
-                }
-            }
-
+          objectTypeDefinitions[objectTypeName] = {
+            description: null,
+            properties: objectTypeProperties,
+          };
         }
-    }
-    )
-    return objectTypeDefinitions
+      }
+    },
+  );
+  return objectTypeDefinitions;
 }
 
 /**
@@ -195,79 +227,94 @@ export async function getObjectTypeDefinitionsFromJSONSchema(containerJSONSchema
    * @param {number} nRows - Number of rows to be read per container to infer the schema of the container.
    * @returns {Promise<CollectionsSchema} Schema of the collections (containers) present in the specified `database`.
 */
-async function getCollectionsSchema(database: Database, nRows: number): Promise<CollectionsSchema> {
+async function getCollectionsSchema(
+  database: Database,
+  nRows: number,
+): Promise<CollectionsSchema> {
+  let collectionDefinitions: CollectionDefinitions = {};
 
-    let collectionDefinitions: CollectionDefinitions = {};
+  let objectTypeDefinitions: ObjectTypeDefinitions = {};
 
-    let objectTypeDefinitions: ObjectTypeDefinitions = {};
+  const scalarTypeDefinitions: ScalarTypeDefinitions = getJSONScalarTypes();
 
-    const scalarTypeDefinitions: ScalarTypeDefinitions = getJSONScalarTypes();
+  const { resources: allContainers } = await database.containers
+    .readAll()
+    .fetchAll();
 
-    const { resources: allContainers } = await database.containers.readAll().fetchAll();
+  for (const container of allContainers) {
+    const dbContainer = database.container(container.id);
 
+    const nContainerRows = await fetchLatestNRowsFromContainer(
+      nRows,
+      dbContainer,
+    );
+    nContainerRows.reverse();
+    const containerJsonSchema = await inferJSONSchemaFromContainerRows(
+      nContainerRows,
+      container.id,
+    );
 
-    for (const container of allContainers) {
-        const dbContainer = database.container(container.id);
+    const containerObjectTypeDefinitions =
+      await getObjectTypeDefinitionsFromJSONSchema(containerJsonSchema);
 
-        const nContainerRows = await fetchLatestNRowsFromContainer(nRows, dbContainer);
-        nContainerRows.reverse();
-        const containerJsonSchema = await inferJSONSchemaFromContainerRows(nContainerRows, container.id);
-
-        const containerObjectTypeDefinitions = await getObjectTypeDefinitionsFromJSONSchema(containerJsonSchema);
-
-        const collectionObjectType: NamedObjectTypeDefinition = {
-            type: "named",
-            name: containerJsonSchema.$ref.split('/').pop() as string,
-            kind: "object"
-        };
-
-        const collectionDefinition: CollectionDefinition = {
-            description: null,
-            arguments: [],
-            resultType: collectionObjectType
-        };
-
-        objectTypeDefinitions = { ...objectTypeDefinitions, ...containerObjectTypeDefinitions };
-        collectionDefinitions[container.id] = collectionDefinition;
-    }
-
-    let schema = {
-        collections: collectionDefinitions,
-        objectTypes: objectTypeDefinitions,
-        scalarTypes: scalarTypeDefinitions,
+    const collectionObjectType: NamedObjectTypeDefinition = {
+      type: "named",
+      name: containerJsonSchema.$ref.split("/").pop() as string,
+      kind: "object",
     };
 
-    return schema
+    const collectionDefinition: CollectionDefinition = {
+      description: null,
+      arguments: [],
+      resultType: collectionObjectType,
+    };
 
+    objectTypeDefinitions = {
+      ...objectTypeDefinitions,
+      ...containerObjectTypeDefinitions,
+    };
+    collectionDefinitions[container.id] = collectionDefinition;
+  }
+
+  let schema = {
+    collections: collectionDefinitions,
+    objectTypes: objectTypeDefinitions,
+    scalarTypes: scalarTypeDefinitions,
+  };
+
+  return schema;
 }
 
 export async function generateConnectorConfig(outputConfigDir: string) {
-    const rowsToFetch = process.env["AZURE_COSMOS_NO_OF_ROWS_TO_FETCH"] ?? "100";
+  const rowsToFetch = process.env["AZURE_COSMOS_NO_OF_ROWS_TO_FETCH"] ?? "100";
 
-    try {
-        const client = constructCosmosDbClient();
-        const schema = await getCollectionsSchema(client.dbClient, parseInt(rowsToFetch));
-        const cosmosKey = client.connectionDetails.key;
-        const cosmosEndpoint = client.connectionDetails.endpoint;
-        const cosmosDbName = client.connectionDetails.databaseName;
+  try {
+    const client = constructCosmosDbClient();
+    const schema = await getCollectionsSchema(
+      client.dbClient,
+      parseInt(rowsToFetch),
+    );
+    const cosmosKey = client.connectionDetails.key;
+    const cosmosEndpoint = client.connectionDetails.endpoint;
+    const cosmosDbName = client.connectionDetails.databaseName;
 
-        const response: any = {
-            connection: {
-                endpoint: cosmosEndpoint,
-                key: cosmosKey,
-                databaseName: cosmosDbName
-            },
-            schema
-        };
+    const response: any = {
+      connection: {
+        endpoint: cosmosEndpoint,
+        key: cosmosKey,
+        databaseName: cosmosDbName,
+      },
+      schema,
+    };
 
-        const writeFile = promisify(fs.writeFile);
+    const writeFile = promisify(fs.writeFile);
 
-        await writeFile(`${outputConfigDir}/config.json`, JSON.stringify(response, null, 2));
-
-
-    } catch (error) {
-        console.log("Error while generating the config", error);
-        exit(1)
-    }
-
+    await writeFile(
+      `${outputConfigDir}/config.json`,
+      JSON.stringify(response, null, 2),
+    );
+  } catch (error) {
+    console.log("Error while generating the config", error);
+    exit(1);
+  }
 }
