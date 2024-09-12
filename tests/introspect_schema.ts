aca7726f49341b367d37704f744c559f7c3696be
Karthikeyan C <karthikeyan@hasura.io>
2024-09-12 17:10:04 +0530

    fix broken npm run test

---
+++ tests/introspect_schema.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import {
+  getObjectTypeDefinitionsFromJSONSchema,
+  inferJSONSchemaFromContainerRows,
+} from "../src/cli/config";
+
+describe("Infer schema of JSON value", function () {
+  it("infers the schema of a simple object", async function () {
+    let simpleObject = `{
+            a: "b"
+        }`;
+    let inferredJSONSchema = await inferJSONSchemaFromContainerRows(
+      [simpleObject],
+      "simple",
+    );
+    let objectTypeDefns =
+      getObjectTypeDefinitionsFromJSONSchema(inferredJSONSchema);
+
+    expect("1", "1");
+  });
+});
